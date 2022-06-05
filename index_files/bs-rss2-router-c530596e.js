aa5e83621a128c26ed44725d4f71679fe971aea7
asahi417 <asahi1992ushio@gmail.com>
2022-06-05 19:14:04 +0100

    update

---
+++ index_files/bs-rss2-router-c530596e.js
@@ -0,0 +1,2 @@
+define("@widget/RSS/bs-rss2-router-c530596e.js",["exports","~/c/bs-router","~/bs-rss2-rssFeeds"],(function(e,s,t){"use strict";class r extends s.R{constructor(){super(...arguments),this.RssFeeds=t.default,this.imgParams="rs=w:540"}}e.default=r,Object.defineProperty(e,"__esModule",{value:!0})})),"undefined"!=typeof window&&(window.global=window);
+//# sourceMappingURL=bs-rss2-router-c530596e.js.map
